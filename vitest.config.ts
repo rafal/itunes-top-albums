6725975a58196a5db21bf16ae4e76dda2235ef67
Rafał Płaszczyk <rafal.plaszczyk@neoncu.be>
2025-03-10 02:19:09 +0100

    fix tests

---
+++ vitest.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vitest/config';
+import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [vue()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  }
+});
